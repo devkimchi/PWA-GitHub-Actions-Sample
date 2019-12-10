8164046e6aeabc3b57c9763d3997ddb42b205bc6
Justin Yoo <justin.yoojh@gmail.com>
2019-12-10 14:18:16 +1100

    Update 404 page

---
+++ src/WebApp/src/pages/404.vue
@@ -0,0 +1,14 @@
+<template>
+  <Layout>
+    <h1>Page Not Found</h1>
+    <p>Ooops</p>
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: 'Page not found'
+  }
+}
+</script>
